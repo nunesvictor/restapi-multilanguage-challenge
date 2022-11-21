3d6d814aa61bc8806b510793fd7ec4231702d8f0
Victor Nunes <nunessvictorr@gmail.com>
2022-11-21 05:29:31 -0300

    feat: add test generic modules

---
+++ go/controllers/controllers_test.go
@@ -0,0 +1,33 @@
+package controllers_test
+
+import (
+	"gin-restapi/database"
+	"gin-restapi/models"
+	"gin-restapi/router"
+	"log"
+	"net/http/httptest"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	r *gin.Engine
+	w *httptest.ResponseRecorder
+)
+
+func setup() {
+	gin.SetMode(gin.ReleaseMode)
+
+	r = router.SetupRouter(database.ConnectTest)
+	w = httptest.NewRecorder()
+
+	database.DB.Create(&models.Autor{Nome: "Fulano", Sobrenome: "de Tal"})
+	database.DB.Create(&models.Editora{Nome: "Editora A", Localidade: "Palmas"})
+}
+
+func shutdown() {
+	if err := os.Remove("test.db"); err != nil {
+		log.Panic("Erro ao excluir arquivo de banco de dados")
+	}
+}
